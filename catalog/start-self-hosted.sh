ddbace579a64f9116a2480a9efc8e4d06b80b936
Your Name <you@example.com>
2023-10-04 09:54:44 +0000

    asd

---
+++ catalog/start-self-hosted.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dapr run --app-id catalog --app-port 5016 --dapr-http-port 3501 --resources-path ../dapr/components dotnet run
---
+++ ordering/start-self-hosted.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dapr run --app-id ordering --app-port 5293 --dapr-http-port 3502 --resources-path ../dapr/components dotnet run
